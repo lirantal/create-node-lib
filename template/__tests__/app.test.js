21a6acdf2aa79d480e7216db504a84650b92abdd
Liran Tal <liran.tal@gmail.com>
2021-11-04 23:41:30 +0200

    feat: re-haul and updates to keep up to date

--- template/__tests__/app.test.js
+++ template/__tests__/app.test.js
@@ -1,3 +1,5 @@
-test('true is true', () => {
-  expect(true).toBe(true)
+import { add } from '../src/main.js'
+
+test('1 + 1 is 2', () => {
+  expect(add(1, 1)).toBe(2)
 })
--- template/index.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-
-const app = require('./src')
-
-module.exports = {}
