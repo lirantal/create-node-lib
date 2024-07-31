f96ee9e2130951650ea8c2d47dbcc873461b4675
Liran Tal <liran.tal@gmail.com>
2024-07-31 20:50:35 +0300

    fix: update eslint setup for correct definitions

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -1,36 +1,32 @@
-import nodePlugin from 'eslint-plugin-n'
 import pluginSecurity from 'eslint-plugin-security'
-import neostandard, { resolveIgnoresFromGitignore } from 'neostandard'
+import neostandard, { resolveIgnoresFromGitignore, plugins } from 'neostandard'
 
-export default neostandard(
+export default [
+  ...neostandard({ ignores: resolveIgnoresFromGitignore() }),
+  plugins.n.configs['flat/recommended-script'],
+  pluginSecurity.configs.recommended,
   {
-    ignores: resolveIgnoresFromGitignore(),
-  },
-  [
-    nodePlugin.configs['flat/recommended-script'],
-    pluginSecurity.configs.recommended,
-    {
-      rules: {
-        'no-process-exit': 'warn',
-        'node/no-unsupported-features': 'off',
-        'node/no-unpublished-require': 'off',
-        'security/detect-non-literal-fs-filename': 'error',
-        'security/detect-unsafe-regex': 'error',
-        'security/detect-buffer-noassert': 'error',
-        'security/detect-child-process': 'error',
-        'security/detect-disable-mustache-escape': 'error',
-        'security/detect-eval-with-expression': 'error',
-        'security/detect-no-csrf-before-method-override': 'error',
-        'security/detect-non-literal-regexp': 'error',
-        'security/detect-object-injection': 'warn',
-        'security/detect-possible-timing-attacks': 'error',
-        'security/detect-pseudoRandomBytes': 'error',
-        'space-before-function-paren': 'off',
-        'object-curly-spacing': 'off',
-      },
-      languageOptions: {
-        ecmaVersion: 2024,
-        sourceType: 'module',
-      },
+    rules: {
+      'no-process-exit': 'warn',
+      'node/no-unsupported-features': 'off',
+      'node/no-unpublished-require': 'off',
+      'security/detect-non-literal-fs-filename': 'error',
+      'security/detect-unsafe-regex': 'error',
+      'security/detect-buffer-noassert': 'error',
+      'security/detect-child-process': 'error',
+      'security/detect-disable-mustache-escape': 'error',
+      'security/detect-eval-with-expression': 'error',
+      'security/detect-no-csrf-before-method-override': 'error',
+      'security/detect-non-literal-regexp': 'error',
+      'security/detect-object-injection': 'warn',
+      'security/detect-possible-timing-attacks': 'error',
+      'security/detect-pseudoRandomBytes': 'error',
+      'space-before-function-paren': 'off',
+      'object-curly-spacing': 'off',
     },
-  ])
+    languageOptions: {
+      ecmaVersion: 2024,
+      sourceType: 'module',
+    },
+  },
+]
