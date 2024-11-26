40bb572dafc9a13f054afea3040894e17c4f6190
Liran Tal <liran.tal@gmail.com>
2024-11-26 11:00:54 +0000

    feat: update tsconfig

--- template/src/bin/cli.ts
+++ template/src/bin/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { debuglog } from 'node:util'
-import { add } from '../main.js'
+import { add } from '../main.ts'
 
 const debug = debuglog('<%= projectName %>')
 
--- template/tsup.config.ts
+++ template/tsup.config.ts
@@ -1,29 +1,29 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsup'
 
 export default defineConfig([
-    {
-        entryPoints: ['src/main.ts', 'src/bin/cli.ts'],
-        format: ['cjs', 'esm'],
-        dts: true,
-        minify: false,
-        outDir: 'dist/',
-        clean: true,
-        sourcemap: false,
-        bundle: true,
-        splitting: false,
-        outExtension(ctx) {
-		    return { 
-                dts: '.d.ts',
-                js: ctx.format === 'cjs' ? '.cjs' : '.mjs'
-            };
-		},
-        treeshake: false,
-        target: 'es2022',
-        platform: 'node',
-        tsconfig: './tsconfig.json',
-        cjsInterop: true,
-        keepNames: true,
-        skipNodeModulesBundle: false,
+  {
+    entryPoints: ['src/main.ts', 'src/bin/cli.ts'],
+    format: ['cjs', 'esm'],
+    dts: true,
+    minify: false,
+    outDir: 'dist/',
+    clean: true,
+    sourcemap: false,
+    bundle: true,
+    splitting: false,
+    outExtension (ctx) {
+      return {
+        dts: '.d.ts',
+        js: ctx.format === 'cjs' ? '.cjs' : '.mjs',
+      }
     },
-    
-]);
+    treeshake: false,
+    target: 'es2022',
+    platform: 'node',
+    tsconfig: './tsconfig.json',
+    cjsInterop: true,
+    keepNames: true,
+    skipNodeModulesBundle: false,
+  },
+
+])
