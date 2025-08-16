9feda25db5ec64bff00b729035457e52b1cd799c
Liran Tal <liran.tal@gmail.com>
2025-08-16 13:37:15 +0300

    fix: defaults for CLIs

--- template/eslint.config.js
+++ template/eslint.config.js
@@ -11,10 +11,10 @@ export default [
   pluginSecurity.configs.recommended,
   {
     rules: {
-      'n/no-process-exit': 'warn',
+      'n/no-process-exit': 'off',
       'n/no-unsupported-features': 'off',
       'n/no-unpublished-require': 'off',
-      'security/detect-non-literal-fs-filename': 'error',
+      'security/detect-non-literal-fs-filename': 'off',
       'security/detect-unsafe-regex': 'error',
       'security/detect-buffer-noassert': 'error',
       'security/detect-child-process': 'error',
@@ -22,12 +22,14 @@ export default [
       'security/detect-eval-with-expression': 'error',
       'security/detect-no-csrf-before-method-override': 'error',
       'security/detect-non-literal-regexp': 'error',
-      'security/detect-object-injection': 'warn',
+      'security/detect-object-injection': 'off',
       'security/detect-possible-timing-attacks': 'error',
       'security/detect-pseudoRandomBytes': 'error',
       'space-before-function-paren': 'off',
       'object-curly-spacing': 'off',
-      'n/hashbang': 'off'
+      'no-control-regex': 'off',
+      'n/hashbang': 'off',
+      'n/no-unsupported-features/node-builtins': 'warn'
     },
     languageOptions: {
       ecmaVersion: 2024,
